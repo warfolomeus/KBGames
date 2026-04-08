7220e51154e38819decd68b150e6d23777ae1c63
Данил Пушин <takeshiimho@gmail.com>
2026-04-08 14:44:37 +0300

    фикс bat

--- run_site.bat
+++ run_site.bat
@@ -1,3 +1,12 @@
 @echo off
+cd /d "%~dp0"
+
+if not exist "node_modules\" (
+    echo [SYSTEM] Libraries not found. Installing...
+    npm install
+)
+
+echo [SYSTEM] Starting dev server...
 npm run dev
+
 pause
